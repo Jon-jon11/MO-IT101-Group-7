f370dd0f0ec008a81848647c68e3ecc47e23521a
appanlilio <lr.apanlilio@mmdc.mcl.edu.ph>
2025-02-08 17:00:23 +0800

    more edits
    should match the package of jon locally

--- Motorphbasicstructure.java
+++ Motorphbasicstructure.java
@@ -1,5 +1,4 @@
-//package motorph_basicstructure; --comment out for now
-
+package motorphbasicstructure;
 import java.util.Scanner;
 
 /**
@@ -8,22 +7,22 @@ import java.util.Scanner;
  */
 
 public class Motorphbasicstructure {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         //login page
         System.out.println("Welcome to MotorPH Employee Portal");
         System.out.print("Username: ");
         String usr_id = scan.nextLine();
-        System.out.print(System.lineSeparator() + "Password: ");
+        System.out.print("Password: ");
         String usr_pw = scan.nextLine();
 
         //welcome Message to our user
         System.out.println("\n\n.\n..\n...\n\nHello, " + usr_id + " !");
         
         //landing page
-        System.out.println("\nWelcome to MotorPH Employee Portal");
-        System.out.print("Enter Employee ID: ");
+        System.out.println("\nWelcome to MotorPH Employee Portal.");
+        System.out.print("Please enter an Employee ID: ");
         String emp_id = scan.nextLine();
 
         //draft employee page
@@ -35,9 +34,10 @@ public class Motorphbasicstructure {
         String emp_hours = scan.nextLine();
 
         //draft working hours page
-        System.out.println("\n\nShowing details for " + emp_hours + " hours");
+        System.out.println("\n\nShowing salary details for employee " + emp_id + ", with " + emp_hours + " hours worked this week");
         System.out.println("Gross Weekly Salary: P21,428.40");
         System.out.println("Net Weekly Salary: P14,854.54");
+        
     }
 
     //blank the screen function para maganda tignan later on -- there are better ways to do this as well this is just incase we're running it purely sa ide console
